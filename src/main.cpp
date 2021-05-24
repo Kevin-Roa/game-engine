b52a38c5dab9b69943e33b9dd035e896749f4d19
Kevin Roa <kevinroa101@gmail.com>
2021-05-24 09:53:07 -0400

    Initial setup

---
+++ src/main.cpp
@@ -0,0 +1,29 @@
+#include "pch.h"
+
+int main() {
+    glfwInit();
+    glfwWindowHint(GLFW_RESIZABLE, true);
+    GLFWwindow* window = glfwCreateWindow(800, 600, "Game Engine", NULL, NULL);
+    glfwMakeContextCurrent(window);
+    
+    if (!window) {
+        glfwTerminate();
+        return -1;
+    }
+    
+    glewExperimental = true;
+    glewInit();
+    glClearColor(0.0f, 1.0f, 1.0f, 1.0f);
+    
+    while(!glfwWindowShouldClose(window)) {
+        glClear(GL_COLOR_BUFFER_BIT);
+        
+		// Code goes here
+		
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+    
+    glfwTerminate();
+    return 0;
+}
